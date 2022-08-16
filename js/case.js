051e9ecef98aaf2328601e58e6ae42062a4e941a
Programming Hero <programming.hero1@gmail.com>
2022-08-15 21:48:53 -0500

    added case plus and minus

---
+++ js/case.js
@@ -0,0 +1,40 @@
+/* 
+1. add event listener to the case plus button
+2. get the value inside the case number field (input field)
+3. convert the number to an integer
+4. calculate the new case number
+5. set the value to the case number field
+
+*/
+
+function updateCaseNumber(isIncrease){
+    const caseNumberField = document.getElementById('case-number-field');
+    const caseNumberString = caseNumberField.value;
+    const previousCaseNumber = parseInt(caseNumberString);
+
+    let newCaseNumber;
+
+    if(isIncrease === true){
+        newCaseNumber = previousCaseNumber + 1;
+    }
+    else{
+        newCaseNumber = previousCaseNumber - 1;
+    }
+
+    caseNumberField.value = newCaseNumber;
+
+    return newCaseNumber;
+}
+
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    const newCaseNumber = updateCaseNumber(true);
+
+    const caseTotalPrice = newCaseNumber * 59;
+    const caseTotalElement = document.getElementById('case-total');
+    caseTotalElement.innerText = caseTotalPrice;
+});
+
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    updateCaseNumber(false);
+})
